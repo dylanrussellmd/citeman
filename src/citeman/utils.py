7894653ed5bd44493176e148378e3d92b39fd615
Dylan Russell <dyl.russell@gmail.com>
2024-05-16 10:41:41 -1000

    working on testing

--- src/citeman/utils.py
+++ src/citeman/utils.py
@@ -2,7 +2,10 @@ import re
 
 def removeBraces(string):
     """
-    Removes ending braces from the given string.
+    Removes starting and ending braces from the given string.
+    Only removes up to a single pair of outer braces.
+    Does not requires a matching pair to remove.
+    Will remove a singlar starting or ending braces.
 
     Args:
         string (str): The input string.
@@ -13,4 +16,15 @@ def removeBraces(string):
     return re.sub(r'^\{?(.*?)\}?$', r'\1', string)
 
 def removeAt(s):
+    """
+    Removes leading '@' characters from the given string.
+    Will remove all leading '@' characters.
+    Does not remove non-leading '@' characters.
+
+    Args:
+        s (str): The input string.
+
+    Returns:
+        str: The string with leading '@' characters removed.
+    """
     return re.sub(r'^@+', '', s)
