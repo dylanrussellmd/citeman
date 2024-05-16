bd89005fcbb0cc9fba9b0f36221bb98fb6f0d9bf
Dylan Russell <dyl.russell@gmail.com>
2024-05-16 12:30:09 -1000

    main menu gui changes

--- src/citeman/ui.py
+++ src/citeman/ui.py
@@ -1,5 +1,7 @@
-from consolemenu import ConsoleMenu
+from consolemenu import ConsoleMenu, MenuFormatBuilder
 from consolemenu.items import FunctionItem
+from consolemenu.menu_component import Dimension
+from colors import blue
 from .ui_history import showQueries
 from .ui_remove import removeCitations
 from .ui_show import showCitations
@@ -11,16 +13,31 @@ def logo():
     logo_path = pkg_resources.resource_filename(__name__, 'logo')
     with open(logo_path, 'r', encoding="utf-8") as f:
         return ''.join([line for line in f])
+    
+class MainMenu(ConsoleMenu):
+    def __init__(self):
+        formatter = MenuFormatBuilder(max_dimension=Dimension(width=100, height = 50))
+        formatter.set_border_style_type(3)
+        formatter.set_title_align("center")
+        formatter.set_subtitle_align("center")
+        formatter.set_prompt("Select: ")
+        title = logo()
+        subtitle = f"A simple {blue('command line citation manager')} for your academic manuscript."
+        super().__init__(title=title,
+                         subtitle=subtitle,
+                         show_exit_option=False,
+                         formatter=formatter)
+
 
 def mainMenu():
     library = prepare_library()
     processor = prepare_processor(library)
-    subtitle = f"A simple command line citation manager for your academic manuscript."
-    menu = ConsoleMenu(logo(), subtitle, show_exit_option=False)
+    #subtitle = f"A simple {blue('command line citation manager')} for your academic manuscript."
+    menu = MainMenu()
     
     menu.append_item(FunctionItem("Query", queryInput, [processor]))
-    menu.append_item(FunctionItem("Show Citations", showCitations, [processor]))
-    menu.append_item(FunctionItem("Remove Citations", removeCitations, [processor]))
-    menu.append_item(FunctionItem("Show Historical Queries", showQueries, [processor]))
+    menu.append_item(FunctionItem("Show citations", showCitations, [processor]))
+    menu.append_item(FunctionItem("Remove citations", removeCitations, [processor]))
+    menu.append_item(FunctionItem("View query history", showQueries, [processor]))
 
     menu.show()
--- src/citeman/ui_notice.py
+++ src/citeman/ui_notice.py
@@ -4,5 +4,6 @@ def noticeScreen(message, color):
     pu = PromptUtils(Screen())
     pu.clear()
     pu.println(color(message))
+    pu.println()
     pu.enter_to_continue()
     pu.clear()
